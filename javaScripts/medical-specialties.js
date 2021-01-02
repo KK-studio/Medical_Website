ec01ec3388f59f64f04d4635ee01ce98c2277080
kash <amkkashani@gmail.com>
2021-01-02 17:27:11 +0330

    par3 from phase js is completed

---
+++ javaScripts/medical-specialties.js
@@ -0,0 +1,23 @@
+console.log("java script is loaded")
+main()
+
+
+
+function main(){
+    // console.log("we run the main")
+    var buttons = document.getElementsByClassName("defaultbutton")
+    console.log(buttons.length)
+
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', redirectToDoctorsPage);
+    }
+}
+
+
+
+
+
+function redirectToDoctorsPage(){
+    window.location.href = "neurologist.html";
+}
+
