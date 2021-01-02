e44023269929dff011de068d7557e578e8b957fc
danial <danial.kordmodanloou@gmail.com>
2021-01-02 23:08:15 +0330

    applying data received to page almost done

--- dr-Page/DrPage.js
+++ dr-Page/DrPage.js
@@ -1,4 +1,54 @@
 API = "https://intense-ravine-40625.herokuapp.com/doctors/"
+Number = ""
+Name = ""
+spec = ""
+avatar = ""
+online_pay = false
+first_empty_date = ""
+experience_years = ""
+stars = 0
+rate = ""
+commenter = ""
+comments = ""
+comment_text = ""
+address = ""
+phone = ""
+week_days = ""
+
+function applyData() {
+    picElement = document.getElementById("DrPic")
+    picElement.src = avatar
+    document.getElementById("name").innerHTML = Name
+    document.getElementById("way-3").innerHTML = " &#62" + Name
+    document.getElementById("technition").innerHTML = spec
+    document.getElementById("way-2").innerHTML = " &#62" + spec
+
+    document.getElementById("number").innerHTML = Number
+    document.getElementById("experience_years").innerHTML = experience_years
+    document.getElementById("first_empty_date").innerHTML = first_empty_date
+    if (online_pay) {
+        document.getElementById("number").innerHTML = "دارد"
+    } else {
+        document.getElementById("number").innerHTML = "ندارد"
+
+    }
+    document.getElementById("comment_text").innerHTML = comment_text
+    var stars1 = document.getElementsByClassName("feather-star")
+    for (i = 0; i < stars; i++) {
+        stars1[i].style.fill = "blue"
+    }
+    for (i = stars; i < stars1.length; i++) {
+        stars1[i].style.fill = "white"
+    }
+    document.getElementById("commenter").innerHTML = commenter
+    document.getElementById("comments").innerHTML = "از " + comments + " نفر"
+    document.getElementById("rate").innerHTML = rate
+    document.getElementById("phone").innerHTML = phone
+    document.getElementById("address").innerHTML = address
+
+
+
+}
 
 function fetchDrPAgeData(ID) {
     URL = API + ID
@@ -7,8 +57,23 @@ function fetchDrPAgeData(ID) {
         .then(
             (input) => {
                 data = input
-                window.alert(data["name"])
+                Number = data["number"]
+                Name = data["name"]
+                spec = data["spec"]
+                avatar = data["avatar"]
+                online_pay = data["online_pay"]
+                first_empty_date = data["first_empty_date"]
+                experience_years = data["experience_years"]
+                stars = data["stars"]
+                rate = data["rate"]
+                commenter = data["commenter"]
+                comments = data["comments"]
+                comment_text = data["comment_text"]
+                address = data["address"]
+                phone = data["phone"]
+                week_days = data["week_days"]
+                applyData()
             })
 
 }
-fetchDrPAgeData("1");
+fetchDrPAgeData("2");
