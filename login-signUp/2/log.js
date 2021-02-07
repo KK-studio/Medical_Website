e97710530113e9942a1dec96d1740045e681de04
danial <danial.kordmodanloou@gmail.com>
2021-02-07 11:40:40 +0330

    some problems in data sending solved

--- login-signUp/2/log.js
+++ login-signUp/2/log.js
@@ -1,4 +1,3 @@
-
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
@@ -6,81 +5,81 @@ const submitLogin = document.getElementById("signInSubmit");
 const submitSignUp = document.getElementById("signUpSubmit");
 
 signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
+    container.classList.add("right-panel-active");
 });
 
 signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
+    container.classList.remove("right-panel-active");
 });
 
-submitLogin.addEventListener('click',(e)=>{
-	console.log("sign in");
-	e.preventDefault();
-	var myForm = document.getElementById("loginForm");
-	
-	var sendData = `{"phone":${myForm.elements[0].value} , "password":${myForm.elements[1].value}}`
-	console.log(sendData);
+submitLogin.addEventListener('click', (e) => {
+    console.log("sign in");
+    e.preventDefault();
+    var myForm = document.getElementById("loginForm");
 
-	makeRequest('POST', "https://www.codegrepper.com/endpoint.php?param1=yoyoma",sendData).then(function(data){
-		if(data == "ok"){
-			console.log("ok")
-		}else{
-			location.reload();
-		}
-  });
+    var sendData = `{"phone":${myForm.elements[0].value} , "password":${myForm.elements[1].value}}`
+    console.log(sendData);
+
+    makeRequest('POST', "http://127.0.0.1:8000/polls/user/signup", sendData).then(function(data) {
+        if (data == "ok") {
+            console.log("ok")
+        } else {
+            location.reload();
+        }
+    });
 });
 
-submitSignUp.addEventListener('click',(e)=>{
-	console.log("sign up");
-	e.preventDefault();
-	var myForm = document.getElementById("loginForm");
-	var sendData = `{"name":${myForm.elements[1].value} , "phone":${myForm.elements[1].value} , "pass":${myForm.elements[2].value}}`
-	console.log(sendData);
+submitSignUp.addEventListener('click', (e) => {
+    console.log("sign up");
+    e.preventDefault();
+    var myForm = document.getElementById("signupForm");
+    var sendData = `{"name":"${myForm.elements[0].value}" , "phone":"${myForm.elements[1].value}" , "password":"${myForm.elements[2].value}"}`
+    console.log(sendData);
 
 
-	makeRequest('POST', "https://www.codegrepper.com/endpoint.php?param1=yoyoma",sendData).then(function(data){
-		if(data == "ok"){
-			console.log("ok")
-		}else{
-			location.reload();
-		}
-		var results=JSON.parse(data);
-		// if(results["state"] == true){
-		// 	//to do  login
-		// }else{
-		// 	location.reload();
-		// }
-  });
+    makeRequest('POST', "http://127.0.0.1:8000/polls/user/signup", sendData).then(function(data) {
+        // window.alert("hi" + data)
+        // if (data == "ok") {
+        //     console.log("ok")
+        // } else {
+        //     location.reload();
+        // }
+        var results = JSON.parse(data);
+        if (results["state"] == true) {
+            //to do  login
+        } else {
+            location.reload();
+        }
+    });
 
 });
 
-function makeRequest (method, url, data) {
-	return new Promise(function (resolve, reject) {
-	  var xhr = new XMLHttpRequest();
-	  xhr.open(method, url);
-	  xhr.onload = function () {
-		if (this.status >= 200 && this.status < 300) {
-		  resolve(xhr.response);
-		} else {
-		  reject({
-			status: this.status,
-			statusText: xhr.statusText
-		  });
-		}
-	  };
-	  xhr.onerror = function () {
-		reject({
-		  status: this.status,
-		  statusText: xhr.statusText
-		});
-	  };
-	  if(method=="POST" && data){
-		  xhr.send(data);
-	  }else{
-		  xhr.send();
-	  }
-	});
-  }
-  
-  //GET example
-  
+function makeRequest(method, url, data) {
+    return new Promise(function(resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.onload = function() {
+            if (this.status >= 200 && this.status < 300) {
+                resolve(xhr.response);
+            } else {
+                reject({
+                    status: this.status,
+                    statusText: xhr.statusText
+                });
+            }
+        };
+        xhr.onerror = function() {
+            reject({
+                status: this.status,
+                statusText: xhr.statusText
+            });
+        };
+        if (method == "POST" && data) {
+            xhr.send(data);
+        } else {
+            xhr.send();
+        }
+    });
+}
+
+//GET example
